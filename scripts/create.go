d3f55131c01d0755e6f9223262e30b92a153dee9
Billy Needham <billyluisneedham@duck.com>
2025-02-01 10:46:34 +0000

    refactor OnFileCreated into its own type

--- scripts/create.go
+++ scripts/create.go
@@ -10,19 +10,21 @@ type MetaData struct {
 	Value string
 }
 
+type OnFileCreated = func(File) error
+
 // TODO write tests for file 
 
-func CreateTodo(title string, onFileCreated func(File) error) (File, error) {
+func CreateTodo(title string, onFileCreated OnFileCreated) (File, error) {
 	now := time.Now()
 	return createFile(title, []string{"todo"}, "", now, false, onFileCreated)
 }
 
-func CreateMeeting(title string, onFileCreated func(File) error) (File, error) {
+func CreateMeeting(title string, onFileCreated OnFileCreated) (File, error) {
 	now := time.Now()
 	return createFile(title, []string{"meeting"}, "", now, true, onFileCreated)
 }
 
-func CreateStandup(getTeamNames func() ([]string, error), onFileCreated func(File) error) (File, error) {
+func CreateStandup(getTeamNames func() ([]string, error), onFileCreated OnFileCreated) (File, error) {
 	teamNames, err := getTeamNames()
 	if err != nil {
 		return File{}, err
@@ -51,7 +53,7 @@ func CreateStandup(getTeamNames func() ([]string, error), onFileCreated func(Fil
 	return createFile(title, []string{"standup"}, content, nextFriday, false, onFileCreated)
 }
 
-func createFile(title string, tags []string, content string, dueAt time.Time, done bool, onFileCreated func(File) error) (File, error) {
+func createFile(title string, tags []string, content string, dueAt time.Time, done bool, onFileCreated OnFileCreated) (File, error) {
 	now := time.Now()
 	date := now.Format("2006-01-02")
 	name := fmt.Sprintf("%v-%v.md", title, date)
