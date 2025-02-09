da348e273f5af54047aa46a563296125d71b037d
Billy Needham <billyluisneedham@duck.com>
2025-02-09 21:05:17 +0000

    chore: remove todo

--- scripts/update.go
+++ scripts/update.go
@@ -3,7 +3,6 @@ package scripts
 
 type WriteFile = func(File) error
 
-// TODO add a get files by name function here
 func DelayDueDate(delayDays int, file File, writeFile WriteFile) error {
 	file.DueAt = file.DueAt.AddDate(0, 0, delayDays)
 	return writeFile(file)
