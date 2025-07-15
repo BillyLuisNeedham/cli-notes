ace55d0bb403903d84d93bd0b6b77e23582f51ee
billy needham <billyluisneedham@duck.com>
2025-07-15 21:54:03 +0100

    feat: open neovim instead of cursor  Please enter the commit message for your changes. Lines starting

--- scripts/presentation/open.go
+++ scripts/presentation/open.go
@@ -1,12 +1,18 @@
 package presentation
 
 import (
+	"os"
 	"os/exec"
 )
 
 
 func OpenNoteInEditor(filePath string) error {
-	err := exec.Command("cursor", filePath).Run()
+	cmd := exec.Command("nvim", filePath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	
+	err := cmd.Run()
 	if err != nil {
 		return err
 	}
